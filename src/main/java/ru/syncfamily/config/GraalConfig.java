41ab74eeb57b5aaba6ebecf30c556abed924eb3a
e.sobolev <evgenii.sobolev@stm-labs.ru>
2026-03-09 12:57:45 +0300

    try to fix build25

--- src/main/java/ru/syncfamily/config/GraalConfig.java
+++ src/main/java/ru/syncfamily/config/GraalConfig.java
@@ -1,27 +1,36 @@
 package ru.syncfamily.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.telegram.telegrambots.meta.api.objects.ApiResponse;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.chat.Chat;
 import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMemberUpdated;
+import org.telegram.telegrambots.meta.api.objects.inlinequery.InlineQuery;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
-import ru.syncfamily.service.model.Product;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 @RegisterForReflection(
+        registerFullHierarchy = true,
         ignoreNested = false,
         targets = {
-        ApiResponse.class,
-        Update.class,
-        Message.class,
-        User.class,
-        Chat.class,
-        ChatMemberUpdated.class,
-        ChatMemberUpdated.ChatMemberUpdatedBuilder.class,
-        Product.class,
-        User.class
+                // Базовые типы ответов
+                ApiResponse.class,
+                Update.class,
+
+                // Все проблемные объекты с билдерами
+                InlineQuery.class,
+                ChatMemberUpdated.class,
+                Message.class,
+                User.class,
+                Chat.class,
+
+                // Регистрируем Jackson типы для работы со списками
+                JsonNode.class,
+                ArrayList.class
 })
 public class GraalConfig {
 }
