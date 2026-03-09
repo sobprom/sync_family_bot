4f70ea840385a9482a42a29a1378d5a9b80be8aa
e.sobolev <evgenii.sobolev@stm-labs.ru>
2026-03-09 11:45:54 +0300

    try to fix build22

--- src/main/java/ru/syncfamily/config/GraalConfig.java
+++ src/main/java/ru/syncfamily/config/GraalConfig.java
@@ -5,16 +5,21 @@ import org.telegram.telegrambots.meta.api.objects.ApiResponse;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.chat.Chat;
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMemberUpdated;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 import ru.syncfamily.service.model.Product;
 
 @SuppressWarnings("unused")
-@RegisterForReflection(targets = {
+@RegisterForReflection(
+        ignoreNested = false,
+        targets = {
         ApiResponse.class,
         Update.class,
         Message.class,
         User.class,
         Chat.class,
+        ChatMemberUpdated.class,
+        ChatMemberUpdated.ChatMemberUpdatedBuilder.class,
         Product.class,
         User.class
 })
